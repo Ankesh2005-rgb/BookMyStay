81d832e5fdbbb86f1e7befdf383ebe53c315e9b8
Ankesh Kumar <your-email@gmail.com>
2026-03-24 21:31:38 +0530

    updated project

--- routes/review.js
+++ routes/review.js
@@ -5,47 +5,13 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
+const reviewController = require("../controllers/reviews.js");
 const { validateReview, isLoggedIn, isreviewAuthor } = require("../middleware.js");
 
 // CREATE
-router.post(
-  "/",
-  isLoggedIn,
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    if (!listing) throw new ExpressError(404, "Listing not found");
-
-    let review = new Review(req.body.review);
-    review.author = req.user._id;
-
-    listing.reviews.push(review);
-
-    await review.save();
-    await listing.save();
-    req.flash("success", "New review created!");
-    res.redirect(`/listings/${listing._id}`);
-  }),
-);
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
 // DELETE
-router.delete(
-  "/:reviewId",
-  isLoggedIn,
-  isreviewAuthor,
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, {
-      $pull: { reviews: reviewId },
-    });
-
-    await Review.findByIdAndDelete(reviewId);
-
-    req.flash("success", "Review deleted!");
-    res.redirect(`/listings/${id}`);
-  }),
-);
+router.delete("/:reviewId", isLoggedIn, isreviewAuthor, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,55 +5,21 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash("success", "Welcome to BookMyStay");
-        res.redirect("/listings");
-      });
-    } catch (err) {
-      req.flash("error", err.message);
-      res.redirect("/signup");
-    }
-  }),
-);
+router.get("/signup", userController.renderSignupForm);
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+router.post("/signup", wrapAsync(userController.signup));
+
+router.get("/login", userController.renderloginForm);
 
 router.post(
   "/login",
   saveRedirectUrl,
   passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }),
-  async (req, res) => {
-    req.flash("success", "Welcome to BookMyStay! You are logged in.");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  },
+  userController.login,
 );
 
-router.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success", "you are logged out!");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
