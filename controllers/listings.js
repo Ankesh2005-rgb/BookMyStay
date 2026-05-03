518a08b8e64519226945b9e721b8f406e3f53e7d
Ankesh Kumar <your-email@gmail.com>
2026-05-03 22:38:33 +0530

    fix mapbox token

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@ const cloudinary = require("../cloudConfig");
 const Listing = require("../models/listing");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 
-const mapToken = process.env.MAP_TOKEN;
+const mapToken = process.env.MAPBOX_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
